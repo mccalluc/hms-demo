13a4be3186945260658208183f338eb5058af7a7
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-05-17 16:34:57 -0400

    ajax query

--- js/init.js
+++ js/init.js
@@ -1,3 +1,10 @@
 $(function () {
-    alert('hello world');
+    var vis = {}; // namespace
+    $.get(
+            'https://dcc.icgc.org/api/v1/projects/GBM-US/mutations?field=id,mutation,type,chromosome,start,end&size=100&order=desc',
+            function (response) {
+                vis.mutations = response.hits;
+                console.log(vis.mutations);
+            }
+    );
 });
