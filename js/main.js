e6ef989ca74d5eca37321fbde8feb5c57e9b7433
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-05-18 01:44:45 -0400

    Make a table, skipping the OO, for now.

--- js/main.js
+++ js/main.js
@@ -2,9 +2,21 @@ $(function () {
     $.get(
             'https://dcc.icgc.org/api/v1/projects/GBM-US/mutations?field=id,mutation,type,chromosome,start,end&size=100&order=desc',
             function (response) {
-                mutations = new Mutations(response.hits);
-                console.log(mutations.type_counts);
-                console.log(mutations.chromo_counts);
+                var $data = $('#data');
+                var hits = response.hits;
+                for (var i = 0; i < hits.length; i++) {
+                    var $tr = $('<tr>');
+                    $('<td>').text(hits[i].id).appendTo($tr);
+                    $('<td>').text(hits[i].mutation).appendTo($tr);
+                    $('<td>').text(hits[i].type).appendTo($tr);
+                    $('<td>').text(hits[i].chromosome).appendTo($tr);
+                    $('<td>').text(hits[i].start).appendTo($tr);
+                    $('<td>').text(hits[i].end).appendTo($tr);
+                    $tr.appendTo($data)
+                       .addClass('chromosome_'+hits[i].chromosome)
+                       .addClass('type_'+hits[i].type.replace(/</g, 'lt').replace(/>/g, 'gt').replace(/\W+/g, '-'));
+                       // TODO: This could result in collisions for some values, but good enough for this data.
+                }
             }
     );
 });
